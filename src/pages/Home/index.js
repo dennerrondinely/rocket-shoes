4aba3f04cc5c5d2e3b63d9984739d607600c4244
Denner Rondinely <denner.rondinely@gmail.com>
2020-04-01 21:15:33 -0300

    WIP

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,116 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-hi/12/D26-0696-012/D26-0696-012_zoom1.jpg?ts=1584643578&ims=544x"
+          alt="Tênis"
+        />
+        <strong>Têbis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-hi/12/D26-0696-012/D26-0696-012_zoom1.jpg?ts=1584643578&ims=544x"
+          alt="Tênis"
+        />
+        <strong>Têbis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-hi/12/D26-0696-012/D26-0696-012_zoom1.jpg?ts=1584643578&ims=544x"
+          alt="Tênis"
+        />
+        <strong>Têbis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-hi/12/D26-0696-012/D26-0696-012_zoom1.jpg?ts=1584643578&ims=544x"
+          alt="Tênis"
+        />
+        <strong>Têbis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-hi/12/D26-0696-012/D26-0696-012_zoom1.jpg?ts=1584643578&ims=544x"
+          alt="Tênis"
+        />
+        <strong>Têbis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-hi/12/D26-0696-012/D26-0696-012_zoom1.jpg?ts=1584643578&ims=544x"
+          alt="Tênis"
+        />
+        <strong>Têbis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-hi/12/D26-0696-012/D26-0696-012_zoom1.jpg?ts=1584643578&ims=544x"
+          alt="Tênis"
+        />
+        <strong>Têbis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
